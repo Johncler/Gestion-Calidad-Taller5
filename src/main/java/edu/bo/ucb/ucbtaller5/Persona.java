5188ec0c48363139acc84be840491d0e341dbcd5
John Clever <john.choque@ucb.edu.bo>
2022-04-25 22:44:58 -0400

    Agregando Taller 5

---
+++ src/main/java/edu/bo/ucb/ucbtaller5/Persona.java
@@ -0,0 +1,12 @@
+
+package edu.bo.ucb.ucbtaller5;
+
+
+public class Persona {
+     private final int MIN = 1;
+    private final int MAX = 3;
+    public int crearPersona(){
+       int piso_persona = (int)Math.floor(Math.random()*(MAX-MIN+1)+MIN);
+       return piso_persona;
+    }
+}
